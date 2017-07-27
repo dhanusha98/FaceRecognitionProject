90cc5d9dfefebda92ba7c5273b34c43082f6e8e6
dhanusha98 <dhanusharoks@hotmail.com>
2017-07-28 01:33:06 +0530

    Face Recognition System
    Face Recognition System using Multi-Layer Perceptron Neural Network

--- FaceRecognitionSystem/FaceRecogOutput.m
+++ FaceRecognitionSystem/FaceRecogOutput.m
@@ -22,7 +22,7 @@ function varargout = FaceRecogOutput(varargin)
 
 % Edit the above text to modify the response to help FaceRecogOutput
 
-% Last Modified by GUIDE v2.5 26-Jul-2017 21:22:59
+% Last Modified by GUIDE v2.5 27-Jul-2017 19:14:25
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -569,6 +569,29 @@ function edit22_Callback(hObject, eventdata, handles)
 % --- Executes during object creation, after setting all properties.
 function edit22_CreateFcn(hObject, eventdata, handles)
 % hObject    handle to edit22 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function edit23_Callback(hObject, eventdata, handles)
+% hObject    handle to edit23 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit23 as text
+%        str2double(get(hObject,'String')) returns contents of edit23 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit23_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit23 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    empty - handles not created until after all CreateFcns called
 
