f0e4ff6819bc496e086a49e5327e5aba63844472
dhanusha98 <dhanusharoks@hotmail.com>
2017-08-06 15:13:17 +0530

    Face Recognition
    Face Recognition System

--- FaceRecognitionSystem/FaceMeasurementCalculation.m
+++ FaceRecognitionSystem/FaceMeasurementCalculation.m
@@ -1,13 +1,13 @@
 %%Face Feature Detector Creation
 
    FaceDetector=vision.CascadeObjectDetector('FrontalFaceCART');
-   ImgFace=imread('1-07.jpg');
+   ImgFace=imread('28-08.jpg');
    ImgFaceResize=imresize(ImgFace, [320,320]);
    bboxFace=step(FaceDetector, ImgFaceResize);
    IFaces=insertObjectAnnotation(ImgFaceResize, 'rectangle', bboxFace, 'Face');
 
    MouthDetector=vision.CascadeObjectDetector('Mouth');
-   ImgMouth=imread('1-07.jpg');
+   ImgMouth=imread('28-08.jpg');
    ImgMouthResize=imresize(ImgMouth, [320,320]);
    MouthDetector.MergeThreshold=40;
    bboxMouth=step(MouthDetector, ImgMouthResize);
@@ -16,7 +16,7 @@
    
    NoseDetector=vision.CascadeObjectDetector('Nose');
    NoseDetector.MergeThreshold=15;
-   ImgNose=imread('1-07.jpg');
+   ImgNose=imread('28-08.jpg');
    ImgNoseResize=imresize(ImgNose, [320,320]);
    bboxNose=step(NoseDetector, ImgNoseResize);
    INose=insertObjectAnnotation(ImgNoseResize, 'rectangle', bboxNose, 'Nose');
@@ -24,20 +24,20 @@
    
    LeftEyeDetector=vision.CascadeObjectDetector('LeftEye');
    LeftEyeDetector.MergeThreshold=40;
-   ImgLeftEye=imread('1-07.jpg');
+   ImgLeftEye=imread('io-fn.jpg');
    ImgLeftEyeResize=imresize(ImgLeftEye, [320,320]);
    bboxLeftEye=step(LeftEyeDetector, ImgLeftEyeResize);
    ILeftEye=insertObjectAnnotation(ImgLeftEyeResize, 'rectangle', bboxLeftEye, 'LeftEye');
 
    
    RightEyeDetector=vision.CascadeObjectDetector('RightEye');
-   RightEyeDetector.MergeThreshold=20;
+   %RightEyeDetector.MergeThreshold=20;
+   RightEyeDetector.MergeThreshold=5;
    ImgRightEye=imread('1-07.jpg');
    ImgRightEyeResize=imresize(ImgRightEye, [320,320]);
    bboxRightEye=step(RightEyeDetector, ImgRightEyeResize);
    IRightEye=insertObjectAnnotation(ImgRightEyeResize, 'rectangle', bboxRightEye, 'RightEye');
    imshow(IRightEye);
-
    
    %%
    
