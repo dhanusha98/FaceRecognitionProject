06f067cf40d52104783f4eb66aae8be3ca21bc32
dhanusha98 <dhanusharoks@hotmail.com>
2017-08-10 16:56:06 +0530

    Face Recognition
    Face Recognition

---
+++ FaceRecognitionSystem/DataAccessLayer.m
@@ -0,0 +1,21 @@
+
+%conn=database('FaceDBConnection', 'root', '');
+%userid=2;
+%useridstring=num2str(userid);
+%selectquery=['SELECT * FROM systemuser WHERE UserID=',useridstring];
+%data=select(conn,selectquery);
+%k=data.UserName;
+
+function x=DataAccessLayer()
+
+userid='1';
+username='DhanushaPS';
+password='19981212dps';
+conn=database('FaceDBConnection', 'root', '');
+
+%validationquery=['SELECT * FROM systemuser WHERE UserName=','''' username '''', 'AND Password=', '''' password ''''];
+validationquery=['SELECT * FROM systemuser WHERE UserName=','''' username ''''];
+datan=select(conn,validationquery);
+assignin('base', 'datan',datan);
+
+end
