6b45ff2a20ab06ee4e32c4fdb54622dbff792c2e
dhanusha98 <dhanusharoks@hotmail.com>
2017-08-25 23:30:12 +0530

    Face Recognition
    Face Recognition

--- FaceRecognitionSystem/Untitled7.m
+++ FaceRecognitionSystem/Untitled7.m
@@ -1,11 +1,19 @@
 
-A=First/max(First);
-B=Second/max(Second);
-C=Third/max(Third);
-D=Fourth/max(Fourth);
-E=Five/max(Five);
-F=Six/max(Six);
-G=Seven/max(Seven);
-H=Eight/max(Eight);
+%A=First/max(First);
+%B=Second/max(Second);
+%C=Third/max(Third);
+%D=Fourth/max(Fourth);
+%E=Five/max(Five);
+%F=Six/max(Six);
+%G=Seven/max(Seven);
+%H=Eight/max(Eight);
 
-Final=[A,B,C,D,E,F,G,H];
+%Final=[A,B,C,D,E,F,G,H];
+%k=max(statsMatrix);
+%D=statsMatrix *10000;
+
+%transponsenet10error=transpose(network10_error);
+%transpose12error=transpose(network12_error);
+
+Input=transpose(statsMatrix);
+
